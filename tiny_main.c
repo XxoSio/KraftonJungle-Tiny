86ec409db8383e49aff8a92f9a81c0745fe8dfcb
Diana <37572031+XxoSio@users.noreply.github.com>
2022-12-09 16:10:05 +0900

    tiny_main.c
    tiny_main.c

---
+++ tiny_main.c
@@ -0,0 +1,19 @@
+/*
+2 * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
+3 * GET method to serve static and dynamic content
+4 */
+#include "csapp.h"
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum,
+char *shortmsg, char *longmsg);
+
+int main(int argc, char **argv)
+{
+  
+}
