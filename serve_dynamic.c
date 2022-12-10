0ca5ccd02bafdfbcaa7d84f682a0508974ec825c
XxoSio <ghdtjgml708@naver.com>
2022-12-11 08:14:12 +0900

    serve_dynamic end;

--- serve_dynamic.c
+++ serve_dynamic.c
@@ -1,22 +1,37 @@
 /*
  * serve_dynamic - run a CGI program on behalf of the client
  */
-/* $begin serve_dynamic */
+#include "csapp.h"
+
 void serve_dynamic(int fd, char *filename, char *cgiargs) 
 {
     char buf[MAXLINE], *emptylist[] = { NULL };
 
     /* Return first part of HTTP response */
+    // 클라이언트에 성공을 알려주는 응답 라인을 보내는 것으로 시작
     sprintf(buf, "HTTP/1.0 200 OK\r\n"); 
     Rio_writen(fd, buf, strlen(buf));
     sprintf(buf, "Server: Tiny Web Server\r\n");
     Rio_writen(fd, buf, strlen(buf));
   
-    if (Fork() == 0) { /* Child */ //line:netp:servedynamic:fork
+    // 새로운 자식의 프로세스를 fork함
+    if (Fork() == 0) { /* Child */
         /* Real server would set all CGI vars here */
-        setenv("QUERY_STRING", cgiargs, 1); //line:netp:servedynamic:setenv
-        Dup2(fd, STDOUT_FILENO);         /* Redirect stdout to client */ //line:netp:servedynamic:dup2
-        Execve(filename, emptylist, environ); /* Run CGI program */ //line:netp:servedynamic:execve
+        // 이때 부모 프로세스는 자식의 PID(process id)를, 자식 프로세스는 0을 반환받음
+        // QUERY_STRING 환경변수를 요청 - URI의 CGI 인자들을 넣겠다는 뜻
+        // 세번째 인자는 기존 환경변수의 유무와 상관없이 값을 변경하겠다면 1, 아니라면 0
+        setenv("QUERY_STRING", cgiargs, 1);
+        /* Redirect stdout to client */
+        // dup2 함수를 통해 표준 출력을 클라이언트와 연계된 연결 식별자로 재지정
+        // -> CGI 프로그램이 표준 출력으로 쓰는 모든 것은 클라이언트로 바로 감(부모프로세스의 간섭 없이)
+        Dup2(fd, STDOUT_FILENO);
+        /* Run CGI program */
+        // CGI 프로그램을 실행 - adder을 실행
+        Execve(filename, emptylist, environ);
     }
-    Wait(NULL); /* Parent waits for and reaps child */ //line:netp:servedynamic:wait
+    
+    /* Parent waits for and reaps child */
+    // 자식이 아니면
+    // 즉, 부모는 자식이 종료되어 정리되는 것을 기다리기 위해 wait함수에서 블록됨
+    Wait(NULL);
 }
